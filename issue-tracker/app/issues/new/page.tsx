88ea9feb6aa76b446ec33671193b757fc3d1295c
adityatyagi <tyagi.aditya844747@gmail.com>
2024-02-28 13:11:03 +0530

    [feat] add react hook form and register the form

--- issue-tracker/app/issues/new/page.tsx
+++ issue-tracker/app/issues/new/page.tsx
@@ -2,18 +2,35 @@
 import { Button, Heading, TextArea, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from "react-hook-form";
 
+// form shape
+interface IssueForm {
+  title: string;
+  description: string;
+}
 const NewIssue = () => {
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+
   return (
-    <div className="max-w-xl space-y-3">
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit((data) => console.log(data))}
+    >
       <Heading>Add New Issue</Heading>
       <TextField.Root>
-        <TextField.Input placeholder="Title" />
+        <TextField.Input placeholder="Title" {...register("title")} />
       </TextField.Root>
-      <SimpleMDE placeholder="Description" />
-      {/* <TextArea placeholder="Description" /> */}
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
+
       <Button>Submit New Issue</Button>
-    </div>
+    </form>
   );
 };
 

bacfd532cc9deb53588268520941d529d134843f
adityatyagi <tyagi.aditya844747@gmail.com>
2024-02-28 11:59:00 +0530

    [feat] added simple markdown editor

--- issue-tracker/app/issues/new/page.tsx
+++ issue-tracker/app/issues/new/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Button, Heading, TextArea, TextField } from "@radix-ui/themes";
-import React from "react";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 const NewIssue = () => {
   return (
@@ -9,7 +10,8 @@ const NewIssue = () => {
       <TextField.Root>
         <TextField.Input placeholder="Title" />
       </TextField.Root>
-      <TextArea placeholder="Description" />
+      <SimpleMDE placeholder="Description" />
+      {/* <TextArea placeholder="Description" /> */}
       <Button>Submit New Issue</Button>
     </div>
   );

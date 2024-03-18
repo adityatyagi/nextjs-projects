f3a77d0ab1209fe19ad59bd000b0390b9f87b1d6
adityatyagi <tyagi.aditya844747@gmail.com>
2024-03-18 15:50:57 +0530

    todos for the auth

--- ai-form-builder/app/providers/index.tsx
+++ ai-form-builder/app/providers/index.tsx
@@ -1,5 +1,7 @@
+// TODO:
 // https://codevoweb.com/setup-and-use-nextauth-in-nextjs-13-app-directory/?source=post_page-----26b9492c227--------------------------------
-
+// https://medium.com/ascentic-technology/authentication-with-next-js-13-and-next-auth-9c69d55d6bfd
+// https://reacthustle.com/blog/extend-user-session-nextauth-typescript
 "use client";
 import { SessionProvider } from "next-auth/react";
 import React, { PropsWithChildren } from "react";

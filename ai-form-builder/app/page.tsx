c6253ea2dc7a7c9c84ebce13e2aae388e74102c5
adityatyagi <tyagi.aditya844747@gmail.com>
2024-03-19 15:51:22 +0530

    fetch forms data from db and show forms list

--- ai-form-builder/app/page.tsx
+++ ai-form-builder/app/page.tsx
@@ -1,12 +1,18 @@
 import Header from "@/components/ui/header";
 import FormGenerator from "./containers/form-generator";
+import { db } from "@/db";
+import { forms } from "@/db/schema";
+import FormsList from "./forms/FormsList";
 
-export default function Home() {
+export default async function Home() {
+  const forms = await db.query.forms.findMany();
+
   return (
     <>
       <Header />
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <FormGenerator />
+        <FormsList forms={forms} />
       </main>
     </>
   );

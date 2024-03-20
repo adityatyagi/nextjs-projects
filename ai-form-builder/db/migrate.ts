16b32943d6c1f12bdd2cb7e47f043a009d5d86f5
adityatyagi <tyagi.aditya844747@gmail.com>
2024-03-20 13:59:24 +0530

    migrate added [wip]

--- ai-form-builder/db/migrate.ts
+++ ai-form-builder/db/migrate.ts
@@ -5,16 +5,15 @@ import { drizzle } from "drizzle-orm/postgres-js";
 
 // Official
 async function main() {
-  console.log("-----migrations start-----");
   const migrationClient = postgres(connectionString, { max: 1 });
   const db = drizzle(migrationClient);
   await migrate(db, { migrationsFolder: "./drizzle/migrations" });
   await migrationClient.end();
-  console.log("****migrations end****");
+  console.log("****migrations successfull****");
   process.exit(0);
 }
 
 main().catch((error) => {
   console.log(error);
-  process.exit(0);
+  process.exit(1);
 });
